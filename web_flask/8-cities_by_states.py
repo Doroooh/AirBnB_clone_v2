d85b346fd04d63e5eee3e143beafb999c72c02ec
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-07-29 00:31:05 +0300

    Web flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask app to generate html list of all states from storage"""
+"""generating html lists of all states from storage"""
 from flask import Flask, render_template
 from models import storage
 app = Flask('web_flask')
@@ -8,17 +8,14 @@ app.url_map.strict_slashes = False
 
 @app.route('/cities_by_states')
 def list_of_states():
-    """Render as html alphabetical list of states and cities in each state
-    from `storage`"""
+    """alphabetical list of states and cities in each state"""
     states = storage.all('State')
     return render_template('8-cities_by_states.html', states=states)
 
-
 @app.teardown_appcontext
 def teardown_db(*args, **kwargs):
-    """Close database or file storage"""
+    """Close the file storage"""
     storage.close()
-
-
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
