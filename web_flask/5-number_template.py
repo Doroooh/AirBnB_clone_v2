e5e7311bf97a67c7c8d3df18d10181b05e25b41f
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-07-29 00:22:54 +0300

    Web flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,38 @@
 #!/usr/bin/python3
-"""Simple Flask web application"""
+"""web app"""
 from flask import Flask, render_template
 app = Flask('web_flask')
 app.url_map.strict_slashes = False
 
-
 @app.route('/')
 def hello_route1():
-    """Return 'Hello HBNB!'"""
+    """displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
-
 
 @app.route('/hbnb')
 def hello_route2():
-    """Return 'HBNB'"""
+    """Displays'HBNB'"""
     return 'HBNB'
-
-
 @app.route('/c/<text>')
 def hello_route3(text):
-    """Return 'C ' followed by text from html request"""
+    """Return 'C ' and the html text"""
     return 'C {}'.format(text.replace('_', ' '))
-
 
 @app.route('/python/<text>')
 @app.route('/python/', defaults={'text': 'is cool'})
 def hello_route4(text):
-    """Return 'Python ' followed by text from html request with
-    default text 'is cool'"""
+    """Display 'Python is cool'"""
     return 'Python {}'.format(text.replace('_', ' '))
 
+@app.route('/number/<int:k>')
+def hello_route5(k):
+    """Display formatted number if can be converted to an int"""
+    return '{:d} is a number'.format(k)
 
-@app.route('/number/<int:n>')
-def hello_route5(n):
-    """Return last part of html request formatted as a number if
-    it can be converted to an int"""
-    return '{:d} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>')
-def hello_route6(n):
-    """Return html template containing the number `n`"""
-    return render_template('5-number.html', n=n)
-
+@app.route('/number_template/<int:k>')
+def hello_route6(k):
+    """Display the html template containing the number `k`"""
+    return render_template('5-number.html', k=k)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
