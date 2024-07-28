e454f08f0aa08676aa626c7e317647a49c5c78b4
Waithira <dorotttyyy@gmail.com>
2024-07-28 04:59:54 -0700

    Web Flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,5 +44,12 @@ def hello_route6(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>')
+def hello_route7(n):
+    """Return rendered html containing logic that determines whether
+    `n` is even or odd and displays the result in an <h1> tag"""
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
