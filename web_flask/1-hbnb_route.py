e20890a4957ebc3061395cfd05531b612757178c
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-07-29 00:08:20 +0300

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,16 +6,14 @@ starting the Flask web app
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """displays Hello HBNB!"""
     return 'Hello HBNB!'
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """displays the phrase HBNB"""
     return 'HBNB'
 
 if __name__ == '__main__':
